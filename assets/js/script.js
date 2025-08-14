3a0ea74cbdb36ca4ff1f50b3e221d842b6b133a3
Rebeljoka <habib.u99@googlemail.com>
2025-08-14 12:02:34 +0100

    Fix: obvious problems that VSC displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ function buttonClicked() {
         messageElement.style.color = 'green';
 
         //Check if input value is > random number and within 1-99 range.
-    } else if (inputValue > randomNumber && inputValue < 100) 
+    } else if (inputValue > randomNumber && inputValue < 100) {
         // Update the guess text and remaining chances
         messageElement.textContent = 'Your guess is high';
         remainingGuesses.textContent = guessesLeft;
@@ -52,7 +52,7 @@ function buttonClicked() {
         messageElement.style.color = '#333';
 
         // If the input value is not within the range of 1 to 99
-     else {
+    } else {
         // Update the guessed number text, color and remaining chances
         messageElement.textContent = 'Your number is invalid';
         remainingGuesses.textContent = guessesLeft;
@@ -63,7 +63,7 @@ function buttonClicked() {
     if (guesses == 0) {
         //Update check button, disable input, and clear input value.
         // Update guessed number text and color to indicate user loss.
-        checkButton.textContent = "Replay';
+        checkButton.textContent = "Replay";
         inputElement.disabled = true;
         inputValue = '';
         messageElement.textContent = 'You lost the game';
@@ -77,4 +77,4 @@ function buttonClicked() {
     // Set the focus on input field
     inputElement.focus();
     inputElement.value = '';
-
+}
